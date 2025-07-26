d424ac85642c880334d38b9cf41970c017545ab6
sri <sri@gmail.com>
2025-07-26 11:42:24 +0530

    Added content in demo.java

---
+++ demo.java
@@ -0,0 +1,8 @@
+public class Demo{
+
+    public static void main (String [] args) {
+    System.out.println ("Hello from Demo");
+         System.out.println ("Hello everyone from demo java here");
+
+    }
+}
