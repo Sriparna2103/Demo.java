0c0a6971de4fc4b473cf3c201cc3fa8ed1d5c60d
Sriparna2103 <83008414+Sriparna2103@users.noreply.github.com>
2025-08-02 15:55:40 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -3,6 +3,6 @@ public class Demo{
     public static void main (String [] args) {
     System.out.println ("Hello from Demo");
          System.out.println ("Hello everyone from demo java here");
-         System.out.println ("Hello World!!!");
+         System.out.println ("Hello Kolkata");
     }
 }
